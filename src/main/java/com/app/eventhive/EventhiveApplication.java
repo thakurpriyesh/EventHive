4be9748111be9ec15fa5b0dd2a57e7bdfd481789
thakurpriyesh <thakurpriyesh01@gmail.com>
2026-09-11 21:59:10 +0530

    Initial commit

---
+++ src/main/java/com/app/eventhive/EventhiveApplication.java
@@ -0,0 +1,13 @@
+package com.app.eventhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/eventhive/EventhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.eventhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
